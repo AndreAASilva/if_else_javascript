6edc07a4b2997703cfc7592cc87a1cfa9f8abc9f
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2022-05-18 14:43:40 -0300

    aula_if_else

---
+++ javascript.js
@@ -0,0 +1,36 @@
+/*function resultado(){
+
+    let res = document.getElementById("aulaifelse")
+    
+    res.innerHTML = "Selecionado uma pessoa Juridica"
+    
+}*/
+
+/*function resultado(){
+
+    let res = document.getElementById("aulaifelse")
+    let res = "juridica"
+
+    if(res === "juridica"){
+
+        res.innerHTML="Ele é pessoa juridica"
+
+    }
+}*/
+
+var pessoa = "FISICA"
+
+if(pessoa === "FISICA"){
+    console.log("Selecionado pessoa física")
+
+
+
+}else if(pessoa === "JURIDICA"){
+    console.log("Selecionado pessoa juridica")
+
+}else{
+    console.log("Tipo de pessoa inválida")
+}
+
+
+

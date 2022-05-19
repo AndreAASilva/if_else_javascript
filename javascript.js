e067be72f71754d45e252139f55765bcf9614998
André Anderson Alves Silva <104483772+AndreAASilva@users.noreply.github.com>
2022-05-19 01:14:31 -0300

    Aula Curso AlgaWorks

--- javascript.js
+++ javascript.js
@@ -1,36 +1,26 @@
 /*function resultado(){
 
     let res = document.getElementById("aulaifelse")
+
     
     res.innerHTML = "Selecionado uma pessoa Juridica"
     
 }*/
 
-/*function resultado(){
+//A função recebe o nome do botão (ONCLICK)
+function resultado(){
 
-    let res = document.getElementById("aulaifelse")
-    let res = "juridica"
+    let res = document.getElementById("aulaifelse")//Aqui a variável foi criada com outro nome mas ela é identificada no HTML como o ID de onde o innerHTML será inscrito
+    let res2 = "juridica"
 
-    if(res === "juridica"){
+    if(res2 === "juridica"){
 
-        res.innerHTML="Ele é pessoa juridica"
+        res.innerHTML="Ele é pessoa juridica<br>"
+        res.innerHTML+="Ele tem CNPJ"
 
+    }else{
+        res.innerHTML="Ele é um pessoa física<br>"
+        res.innerHTML+="Ele tem CPF"
     }
-}*/
-
-var pessoa = "FISICA"
-
-if(pessoa === "FISICA"){
-    console.log("Selecionado pessoa física")
-
-
-
-}else if(pessoa === "JURIDICA"){
-    console.log("Selecionado pessoa juridica")
-
-}else{
-    console.log("Tipo de pessoa inválida")
 }
-
-
 
